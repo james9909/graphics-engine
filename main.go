6ccabc394fe6cfc649d9cc511894d7f2aa60bf0a
James Wang <jameswang9909@hotmail.com>
2017-05-15 11:22:49 -0400

    Add --profile flag

--- main.go
+++ main.go
@@ -1,17 +1,35 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"log"
 	"os"
+	"runtime/pprof"
 )
 
+var profile = flag.Bool("profile", false, "Profile")
+
 func main() {
+	flag.Parse()
+	args := flag.Args()
 	parser := NewParser()
+
+	if *profile {
+		fmt.Println(*profile)
+		f, err := os.Create("cpu.prof")
+		if err != nil {
+			log.Fatal(err)
+		}
+		pprof.StartCPUProfile(f)
+		defer pprof.StopCPUProfile()
+	}
+
 	var err error
-	if len(os.Args) < 2 {
+	if len(args) == 0 {
 		err = parser.ParseInput()
 	} else {
-		err = parser.ParseFile(os.Args[1])
+		err = parser.ParseFile(args[0])
 	}
 	if err != nil {
 		fmt.Fprintln(os.Stderr, "Parse error:", err)
